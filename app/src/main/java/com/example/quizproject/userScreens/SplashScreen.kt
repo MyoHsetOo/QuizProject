a4c140001efcc28ad14664902bdf4cedb6f887f9
zadeveloper <eieikyaw77777@gmail.com>
2023-06-12 14:01:20 +0630

    SplashScreen Commit

--- app/src/main/java/com/example/quizproject/userScreens/SplashScreen.kt
+++ app/src/main/java/com/example/quizproject/userScreens/SplashScreen.kt
@@ -4,12 +4,20 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -20,11 +28,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.quizproject.R
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navController: NavController) {
+
     var startAnimation by remember { mutableStateOf(false) }
     val alphaAnim = animateFloatAsState(
         targetValue = if (startAnimation) 1f else 0f,
@@ -32,31 +50,45 @@ fun SplashScreen() {
             durationMillis = 7000
         ), label = ""
     )
+    Surface(modifier = Modifier.background(color = MaterialTheme.colorScheme.primary)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-    LaunchedEffect(key1 = true) {
-        startAnimation = true
-        delay(7000)
-       // navController.popBackStack()
-       // navController.navigate()
+        LaunchedEffect(key1 = true) {
+            startAnimation = true
+            delay(7000)
+            navController.navigate("HomeScreen")
+        }
+            Splash(alpha = alphaAnim.value)
+                Text(text = "Quiz",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        fontStyle = FontStyle.Italic
+                    )
+                )
+
+        }
+
     }
-    Splash(alpha = alphaAnim.value)
-}
+    }
 
 @Composable
 fun Splash(alpha: Float) {
-    Box(
-        modifier = Modifier
-            .background(if (isSystemInDarkTheme()) Color.White else Color.Yellow)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
+    Surface(modifier = Modifier.background(color = MaterialTheme.colorScheme.primary)) {
+        Box(
             modifier = Modifier
-                .size(120.dp)
-                .alpha(alpha = alpha),
-            imageVector = Icons.Default.Email,
-            contentDescription = "Logo Icon",
-            tint = Color.Black
-        )
+                .background(color = MaterialTheme.colorScheme.secondary)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.quizz)  )
+            LottieAnimation(
+                  modifier = Modifier.fillMaxSize(1f),
+                iterations = 1000, composition = composition
+            )
+
+        }
     }
+
 }
