d8b3aa76a30e35207070b75f46571792d5b11aec
zadeveloper <eieikyaw77777@gmail.com>
2023-06-12 12:42:16 +0630

    SplashScreen Commit

---
+++ app/src/main/java/com/example/quizproject/userScreens/SplashScreen.kt
@@ -0,0 +1,62 @@
+package com.example.quizproject.userScreens
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen() {
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 7000
+        ), label = ""
+    )
+
+    LaunchedEffect(key1 = true) {
+        startAnimation = true
+        delay(7000)
+       // navController.popBackStack()
+       // navController.navigate()
+    }
+    Splash(alpha = alphaAnim.value)
+}
+
+@Composable
+fun Splash(alpha: Float) {
+    Box(
+        modifier = Modifier
+            .background(if (isSystemInDarkTheme()) Color.White else Color.Yellow)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier
+                .size(120.dp)
+                .alpha(alpha = alpha),
+            imageVector = Icons.Default.Email,
+            contentDescription = "Logo Icon",
+            tint = Color.Black
+        )
+    }
+}
