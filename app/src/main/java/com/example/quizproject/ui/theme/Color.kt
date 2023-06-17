4aa5ea88b73518e6c10affd67d6d6c93b3ce6674
zadeveloper <swezinnyein111@gmail.com>
2023-06-17 12:10:44 +0630

    color Commit

--- app/src/main/java/com/example/quizproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/quizproject/ui/theme/Color.kt
@@ -45,7 +45,7 @@ val md_theme_light_scrim = Color(0xFF000000)*/
 
 val md_theme_dark_primary = Color(0xFFB5CFF8)
 
-val Test = Color(0xFFC0D6F8)
+val Test = Color(0xFF415679)
 
 /*val md_theme_dark_onPrimary = Color(0xFF452B00)
 val md_theme_dark_primaryContainer = Color(0xFF633F00)
