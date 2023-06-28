448324ccc6d6cfbf64c0f3426658e6ff33fd2fcb
zadeveloper <myohsetoo23@gmail.com>
2023-06-28 15:38:02 +0630

    Day28L Commit

--- app/src/main/java/com/example/quizproject/dataModel/Item.kt
+++ app/src/main/java/com/example/quizproject/dataModel/Item.kt
@@ -4,7 +4,7 @@ import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.BsonObjectId
 import org.mongodb.kbson.ObjectId
-
+///
 class Item() : RealmObject {
     @PrimaryKey
     var _id: ObjectId = BsonObjectId()
